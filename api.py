dafbce997ad087c092beafc8af03e6fe082b9d61
KCMak653 <k.makulowich@gmail.com>
2025-07-20 19:57:56 -0500

    fix port

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from project_quoter import ProjectQuoter
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app, origins="*")
@@ -67,4 +68,4 @@ def test():
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
